70706c34331b17968a62d28591b541a5ac8b73aa
Projckt <projckt@protonmail.com>
2023-11-07 08:12:01 +0530

    fixes

---
+++ build.sh
@@ -0,0 +1,44 @@
+# Cleanup previous prod
+cd ..
+rm -rf prod
+mkdir prod
+
+# Init Git prod
+cd prod
+git init
+git remote add origin git@github.com-projckt:projckt/starter_website-prod.git
+
+# Pre-build app-server
+cd ..
+cd dev
+cd app-server 
+
+# Build app-server
+npm run build
+rsync -av dest/ ../../prod
+cp {.gitignore,package.json} ../../prod
+rm -rf dest
+
+# Pre-build site-frontend
+cd .. 
+cd site-frontend 
+
+# Build app-frontend
+npm run build 
+rsync -av --delete dist/ ../../prod/www
+rm -rf dist
+
+# Push prod 
+cd ..
+cd ..
+cd prod
+git add --all
+git commit -m "Deploy build `date`"
+git push origin main --force
+
+# Install modules in prod
+npm install 
+
+# Reset
+cd ..
+cd dev
