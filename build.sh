e4fe02f8b37fc6199f4097c2e67411e6a96e1abf
Projckt <projckt@protonmail.com>
2024-03-13 10:04:54 +0530

    fixes build script

--- build.sh
+++ build.sh
@@ -1,27 +1,19 @@
-## Delete previous ./prod 
-cd ..
-rm -rf prod
-mkdir prod
-
-## Init git in ./prod
-cd prod
-git init
-git remote add origin git@github.com-{username}:{username}/{repo_name}-prod.git
-
 ## Build ./frontend
-cd .. 
-cd frontend 
+cd frontend
 npm run build 
 rsync -av --delete dist/ ../../prod
 
-## Push prod 
+# Init git in ./prod
 cd ..
 cd ..
 cd prod
+git init
+git remote add origin git@github.com-sensefolks:sensefolks/website-prod.git
+
+## Push prod 
 git add --all
 git commit -m "Deploy build `date`"
 git push origin main --force
-
 
 ## Reset
 cd ..
