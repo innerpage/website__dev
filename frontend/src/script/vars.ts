55c89693b7e6849dae620777073cc59488c95c5b
Projckt <projckt@protonmail.com>
2024-04-13 14:02:33 +0530

    fixes

--- frontend/src/script/vars.ts
+++ frontend/src/script/vars.ts
@@ -1,6 +1,11 @@
 export const Vars = {
   site: {
-    name: "Website starter",
+    name: "Website Starter",
     description: "This is a starter template",
+    email: "support@sitename.com",
+    url: "https://sitename.com",
+  },
+  app: {
+    url: "https://app.sitename.com",
   },
 };
