8611557f913ab06a73178d5e9c25945d599163c3
Projckt <projckt@protonmail.com>
2024-06-17 10:05:30 +0530

    prepares the website

--- frontend/src/script/var.ts
+++ frontend/src/script/var.ts
@@ -1,27 +1,31 @@
 export const Var = {
   app: {
-    name: "Website Starter",
-    description: "Dev codebase of Website Starter",
-    tagline: "Just another tagline",
+    name: "InnerPage",
+    description: "An Anonymous Journal",
+    tagline: "Your safest place",
     contact: {
-      email: "-",
-      url: "-",
+      email: "innerpage.journal@gmail.com",
+      url: "https://innerpage.org/contact-us",
       social: {
-        twitter: "-",
+        twitter: "https://x.com/TheInnerPage",
       },
     },
-    url: "",
+    url: "https://app.innerpage.org",
     logo: {
       website: {
         rectangle: {
           color: "-",
-          black: "-",
-          white: "-",
+          black:
+            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logotype_Transparent_Black_kvzkty.png",
+          white:
+            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logotype_Transparent_White_sfcqmy.png",
         },
         square: {
           color: "-",
-          black: "-",
-          white: "-",
+          black:
+            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logo_Transparent_Black_dylnfh.png",
+          white:
+            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logo_Transparent_White_b2sfhk.png",
         },
       },
       docs: {
@@ -50,7 +54,7 @@ export const Var = {
       },
     },
     website: {
-      url: "-",
+      url: "https://innerpage.org",
     },
   },
 };
--- readme.md
+++ readme.md
@@ -1,29 +1,35 @@
 ## Clone repo
 
-❌ `git clone git@github.com-projckt:projckt/starter_website-dev.git dev`
-❌ `cd dev`
+✅ `git clone git@github.com-projckt:projckt/starter_website-dev.git dev`
+✅ `cd dev`
+
+<hr/>
 
 ## Edit remote
 
-❌ `git remote set-url origin git@github.com-{username}:{username}/website__dev.git`
+✅ `git remote set-url origin git@github.com-{username}:{username}/website__dev.git`
+
+<hr/>
 
 ## Edit ./build.sh
 
-❌ Change production repo to `git@github.com-{username}:{username}/website__prod.git`
+✅ Change production repo to `git@github.com-{username}:{username}/website__prod.git`
+
+<hr/>
 
 ## Install dependencies
 
-❌ `./init.sh`
+✅ `./init.sh`
+
+<hr/>
 
 ## Edit ./frontend/package.json
 
-❌ Change `name`
-❌ Change `description`
+✅ Change `name`
+✅ Change `description`
 
-## Edit ./frontend/src/layouts/Site.astro
-
-❌ Change `title` prop of `Layout` in pages accordingly
+<hr/>
 
 ## Edit ./frontend/src/script/var.ts
 
-❌ Change everything
+✅ Change everything
