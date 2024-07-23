54db4854f81c6d691afff308d5d5d712f220a473
Projckt <projckt@protonmail.com>
2024-07-23 19:41:24 +0530

    fixes

--- frontend/src/script/var.ts
+++ frontend/src/script/var.ts
@@ -2,8 +2,8 @@ export const Var = {
   app: {
     name: "InnerPage",
     description:
-      "InnerPage is a private digital journal for privacy conscious people. Pour out your deepest thoughts and feelings anonymously",
-    tagline: "Improve your mental health",
+      "InnerPage is a private and secure digital journal for privacy conscious people. Pour out your deepest thoughts and feelings anonymously. Improve your mental health through journaling",
+    tagline: "The private & secure journal to improve your mental health",
     contact: {
       email: "innerpage.journal@gmail.com",
       social: {
