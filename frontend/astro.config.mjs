970ea97404f0b29bd78eb4887dbbc896383e84e6
Projckt <projckt@protonmail.com>
2024-07-24 00:02:43 +0530

    adds google analytics

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import { Var } from "./src/script/var";
 
 // https://astro.build/config
 export default defineConfig({
   site: Var.app.website.url,
-  integrations: [sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
