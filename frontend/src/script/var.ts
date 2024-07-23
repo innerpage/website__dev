3bc3c9c33cfae8fc992c5b3ad0b8f8ae2d0d9737
Projckt <projckt@protonmail.com>
2024-07-23 09:25:41 +0530

    refactors css

--- frontend/src/script/var.ts
+++ frontend/src/script/var.ts
@@ -6,56 +6,10 @@ export const Var = {
     tagline: "Improve your mental health",
     contact: {
       email: "innerpage.journal@gmail.com",
-      url: "https://innerpage.org/contact-us",
       social: {
         twitter: "https://x.com/TheInnerPage",
       },
     },
     url: "https://app.innerpage.org",
-    logo: {
-      website: {
-        rectangle: {
-          color: "-",
-          black:
-            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logotype_Transparent_Black_kvzkty.png",
-          white:
-            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logotype_Transparent_White_sfcqmy.png",
-        },
-        square: {
-          color: "-",
-          black:
-            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logo_Transparent_Black_dylnfh.png",
-          white:
-            "https://res.cloudinary.com/dj9xh37fj/image/upload/v1718521642/Logo_Transparent_White_b2sfhk.png",
-        },
-      },
-      docs: {
-        rectangle: {
-          color: "-",
-          black: "-",
-          white: "-",
-        },
-        square: {
-          color: "-",
-          black: "-",
-          white: "-",
-        },
-      },
-      blog: {
-        rectangle: {
-          color: "-",
-          black: "-",
-          white: "-",
-        },
-        square: {
-          color: "-",
-          black: "-",
-          white: "-",
-        },
-      },
-    },
-    website: {
-      url: "https://innerpage.org",
-    },
   },
 };
