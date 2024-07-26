42911512bd866b7f4938620041ab1587dacf5cb2
Projckt <projckt@protonmail.com>
2024-07-27 00:15:36 +0530

    removes partytown integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import { Var } from "./src/script/var";
 
 // https://astro.build/config
 export default defineConfig({
   site: Var.app.website.url,
-  integrations: [
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [sitemap()],
 });
