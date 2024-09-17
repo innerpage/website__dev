1f6c53330051bf09ab8cdeac461d1f59049b45c5
Projckt <projckt@protonmail.com>
2024-09-17 11:38:51 +0530

    adds terms & conditions

--- frontend/src/script/var.ts
+++ frontend/src/script/var.ts
@@ -1,18 +1,22 @@
 export const Var = {
   app: {
-    name: "InnerPage",
+    name: 'InnerPage',
     description:
-      "Anonymous, Private and Secure Digital Journal for Privacy Conscious People",
-    tagline: "Improve Your Mental Health",
+      'Anonymous, Private and Secure Digital Journal for Privacy Conscious People',
+    tagline: 'Improve Your Mental Health',
     contact: {
-      email: "innerpage.journal@gmail.com",
+      email: 'innerpage.journal@gmail.com',
       social: {
-        twitter: "https://x.com/TheInnerPage",
+        twitter: 'https://x.com/TheInnerPage',
       },
     },
-    url: "https://app.innerpage.org",
+    url: 'https://app.innerpage.org',
     website: {
-      url: "https://innerpage.org",
+      url: 'https://innerpage.org',
+    },
+    developer: {
+      name: 'Tuhin Bhuyan',
+      url: 'https://www.linkedin.com/in/xtbhyn/',
     },
   },
-};
+}
